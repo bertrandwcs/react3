150f0d6d4bb37e4694612478b2b8c495a38504b0
wilder <bertrandwcs>
2019-04-29 15:45:19 +0200

    maj

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Quotes from "./Quotes";
+import Lamp from "./Lamp";
+
+class App extends Component {
+  constructor (){
+    super();
+    this.state ={
+      working:false
+    }
+  }
+
+  workedClick= () => {this.setState({working:!this.state.working})}
+
+  render() {
+    let className = 'App-logo';
+    if (this.state.working) {
+    className += ' working';
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={className} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
+        </header>
+        <Lamp on />
+        <Lamp />
+        <button onClick={this.workedClick}>Donuts?!</button>
+        <Quotes/>
+      </div>
+    );
+  }
 }
 
 export default App;
